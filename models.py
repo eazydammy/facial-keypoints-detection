6e3f0fb2cd56285b531e6cb7eb1c24ecd25e185d
Oluwadamilola Saka <eazydammy@gmail.com>
2019-12-01 19:26:21 -0600

    style: Remove unimportant comment

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-# can use the below import should you choose to initialize the weights of your Net
 import torch.nn.init as I
 
 
